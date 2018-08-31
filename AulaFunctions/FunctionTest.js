20b4137f99708202707c5bd2c33ce3be40d51aae
PotatoCharles <carlosdg100@gmail.com>
2018-08-30 22:38:08 -0300

    AulaFunctions

--- AulaFunctions/FunctionTest.js
+++ AulaFunctions/FunctionTest.js
@@ -4,3 +4,15 @@ function mult(x,y){
 }
 var resultmult = mult("preto","branco");
 alert( resultmult );
+
+function SkullGirls(characters){
+	var sk;
+	for(sk = 0;sk < characters.length;sk++){
+		console.log(characters[sk]);
+	}
+}
+
+var SkullGirlsCharacters = ["Cerebella","Parasoul","Valentine","Eliza","Filia","Squigly","Peacock"];
+
+SkullGirls(SkullGirlsCharacters);
+console.log("Number of Characters in SkullGirlsCharacters :"+SkullGirlsCharacters.length);
